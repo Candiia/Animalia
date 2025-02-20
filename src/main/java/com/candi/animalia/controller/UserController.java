52883d82904b36d19a29a71dc94b18cbadff0742
candi <alcantarilla.cacan24@gmail.com>
2025-02-20 10:36:09 +0100

    intentar ejecutar la base de datos

--- src/main/java/com/candi/animalia/controller/UserController.java
+++ src/main/java/com/candi/animalia/controller/UserController.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
     private final JwtService jwtService;
